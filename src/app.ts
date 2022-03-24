d1452762f2141427e3a3d5b1448a29f11ae065d3
edmini <shinhosan@gmail.com>
2022-03-24 16:27:55 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+
+import express from 'express'
+import { Request, Response, NextFunction } from 'express'
+
+const app = express();
+const port:number = 3000;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).send("Welcome typescript!!")
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
